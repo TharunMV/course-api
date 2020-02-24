9a8694bae44aac137a14262d412abcc7b2f358c2
TharunMV <37066939+TharunMV@users.noreply.github.com>
2020-02-24 20:14:06 +0530

    Add files via upload

---
+++ src/main/java/com/altimetrik/springbasics/TopicService.java
@@ -0,0 +1,45 @@
+package com.altimetrik.springbasics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("java", "Java", "Java Description"),
+			new Topic("python", "Python", "Python Description"),
+			new Topic("javascript", "JavaScript", "JavaScript Description")
+			));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void add(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
